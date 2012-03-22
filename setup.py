5f4f591aef3c9419657ec1f01765aacc4e18ea7c
holm <holm@podio.com>
2012-03-22 14:50:49 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-__version__ = "0.3.2"
+__version__ = "0.3.2b"
 
 setup(
     name='pyfb',
     version=__version__,
     description='A Python Interface to the Facebook Graph API',
     author='Juan Manuel García',
-    author_email = "jmg.utn@gmail.com",
-    license = "GPL v3",
-    keywords = "Facebook Graph API Wrapper Python",
+    author_email="jmg.utn@gmail.com",
+    license="GPL v3",
+    keywords="Facebook Graph API Wrapper Python",
     url='http://code.google.com/p/pyfb/',
     packages=['pyfb'],
     install_requires=[
