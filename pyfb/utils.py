29b922f416d934c70773076fea22f57fe24312e9
holm <holm@podio.com>
2012-04-24 14:13:24 +0200

    Simplify object factory

--- pyfb/utils.py
+++ pyfb/utils.py
@@ -4,15 +4,13 @@
     This file provides utilities to the pyfb library
 """
 
-import simplejson
-
 class NamedObject(object):
     """
         Builds an object of a runtime generated class with a name
         passed by argument.
     """
     def __new__(cls, name):
-        return type(str(name), (object, ), {})
+        return type(str(name), (object,), {})
 
 
 class Json2ObjectsFactory(object):
@@ -23,35 +21,20 @@ class Json2ObjectsFactory(object):
         everything into an object.
     """
 
-    def loads(self, data):
-        return simplejson.loads(data)
-
-    def make_object(self, name, data):
-        raw = simplejson.loads(data)
-        return self._make_object(name, raw)
-
-    def make_objects_list(self, name, data):
-        raw = simplejson.loads(data)
-        return self._make_objects_list(name, raw)
-
     def _make_objects_list(self, name, values):
-        objs = []
-        for data in values:
-            if isinstance(data, dict):
-                objs.append(self._make_object(name, data))
-            else:
-                objs.append(data)
-        return objs
+        return [self._make_object(name, value) for value in values]
 
-    def _make_object(self, name, dic):
+    def _make_object_dict(self, name, dic):
         #Life's easy. For Python Programmers BTW ;-).
         obj = NamedObject(name)
         for key, value in dic.iteritems():
-            if key == 'data':
-                key = obj.__name__
-            if isinstance(value, list):
-                value = self._make_objects_list(key, value)
-            elif isinstance(value, dict):
-                value = self._make_object(key, value)
-            setattr(obj, key, value)
+            setattr(obj, key, self._make_object(name, obj))
         return obj
+
+    def make_object(self, name, obj):
+        if isinstance(obj, dict):
+            return self._make_object_dict(name, obj)
+        elif isinstance(obj, list):
+            return self._make_objects_list(name, obj)
+        else:
+            return obj
