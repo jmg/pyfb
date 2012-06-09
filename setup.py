dd63fca61a51b6c67876a4cfca9b125e34977490
Pedro Rodrigues <medecau@gmail.com>
2012-06-09 10:00:13 +0100

    pyfb.__version__ did not match setup.py __version__

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-
-__version__ = "0.3.5"
+from pyfb import __author__, __version__, __license__
 
 setup(
     name='pyfb',
     version=__version__,
     description='A Python Interface to the Facebook Graph API',
-    author='Juan Manuel García',
+    author=__author__,
     author_email = "jmg.utn@gmail.com",
-    license = "GPL v3",
+    license = __license__,
     keywords = "Facebook Graph API Wrapper Python",
     url='http://code.google.com/p/pyfb/',
     packages=['pyfb'],
