6e01fca1418430d91e42866ec8c4969a40302afe
Juan Manuel García <jmg.utn@gmail.com>
2012-06-11 15:38:47 -0300

    fixed tests

--- test.py
+++ test.py
@@ -3,19 +3,19 @@ from pyfb import Pyfb
 from testdata import *
 
 class pyfbTests(unittest.TestCase):
-    
+
     def setUp(self):
         self.facebook = Pyfb(FACEBOOK_APP_ID)
         self.facebook.set_access_token(FACEBOOK_TOKEN)
         self.me = self.facebook.get_myself()
-    
+
     def test_auth(self):
         self.assertEquals(type(self.me.name), type(unicode()))
-    
+
     def test_get_friends(self):
-        self.assertEquals(type(self.me.get_friends()), type(list()))
-    
+        self.assertEquals(type(self.facebook.get_friends(self.me.id)), type(list()))
+
 if __name__ == "__main__":
 
     unittest.main()
-    
+
